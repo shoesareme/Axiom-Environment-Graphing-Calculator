34d8d04c885df7cc6928b1deafcb3c648c7cd8fa
shoesareme <77761814+shoesareme@users.noreply.github.com>
2023-08-19 21:27:13 -0400

    Create customMath.py
    Skeleton code for main purpose of this project created

---
+++ source/customMath.py
@@ -0,0 +1,21 @@
+# --- Environment --- #
+
+"""
+Environment is a system of axioms.
+
+Old system: Coeff defined by variables
+New systems: Coeff defined in environment
+"""
+
+# --- Base Class --- #
+
+"""
+The most used base class is the integer.
+After that, there is the float.
+"""
+
+class BaseIntegerEnvironment(object):
+    def __init__(self):
+        pass
+
+#--- Different Mathematical/Logical Systems --- #
