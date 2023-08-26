cdbd6aa4b393ad8df0dcfe4874fb60d6698048f1
shoesareme <77761814+shoesareme@users.noreply.github.com>
2023-08-25 20:59:30 -0400

    Cleaned up code
    Code is cleaner and minor adjustments done

--- source/customMath.py
+++ source/customMath.py
@@ -7,6 +7,8 @@ Old system: Coeff defined by variables
 New systems: Coeff defined in environment
 """
 
+# Something IDK
+
 # --- Base Class --- #
 
 """
@@ -18,7 +20,11 @@ Base classes will also have system for float and maybe complex
 class BaseIntegerEnvironment(object):
     def __init__(self):
         self.systemConstant = int  # CONSTANT PER DOCUMENTATION
+
+        self.constants = {}  # replace "aaa" variable algorithm (WTF Algorithm)
+
+        self.variables = {}  # these change, like x and y. constants are coeff.
         
-        self.variables = {}  # replace "aaa" variable algorithm
+        # I would define different templates and stuff but ehh i'm lazy
 
 #--- Different Mathematical/Logical Systems --- #
