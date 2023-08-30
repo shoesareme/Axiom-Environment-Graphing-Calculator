3faff1c63ca2244cf8441d65d034b3018af98d26
shoesareme <77761814+shoesareme@users.noreply.github.com>
2023-08-29 20:59:10 -0400

    Minor migrations complete
    Began migrating items from graph.py to customMath.py.
    Created classes for float environments and more.
    Minor adjustments.

--- source/customMath.py
+++ source/customMath.py
@@ -20,11 +20,65 @@ Base classes will also have system for float and maybe complex
 class BaseIntegerEnvironment(object):
     def __init__(self):
         self.systemConstant = int  # CONSTANT PER DOCUMENTATION
+        self.systemConstantName = "int"  # may become obsolete soon
 
         self.constants = {}  # replace "aaa" variable algorithm (WTF Algorithm)
 
         self.variables = {}  # these change, like x and y. constants are coeff.
         
+        self.acceptedChars = [
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "x",
+            "X",
+            "+",
+            "-",
+            "/",
+            "*",
+            "^",
+            " ",
+            "",
+            ".",
+            "(",  # Deal with these two later
+            ")"  # Deal with these two later
+        ]
+
         # I would define different templates and stuff but ehh i'm lazy
 
+    def addConstant(self, arg, name):
+        self.constants[name] = arg
+
+    def addVariable(self, arg, name):
+        self.variables[name] = arg
+    
+    # --- MIGRATION --- #
+    # BEGIN MIGRATING EQUATIONS AND INTERPRETATION INTO ENVIRONMENTS
+
+
+class FloatEnvironment(BaseIntegerEnvironment):
+    def __init__(self):
+        super(FloatEnvironment, self).__init__(())
+        # get template from previous
+        
+        self.systemConstant = float
+        self.systemConstantName = "float"
+        
+
 #--- Different Mathematical/Logical Systems --- #
+
+class ComplexEnvironmentX(BaseIntegerEnvironment):
+    pass
+
+class ComplexEnvironmentXY(BaseIntegerEnvironment):  # heat map
+    pass
+
+class ModularEnvironment(BaseIntegerEnvironment):  # modular addition and stuf
+    pass
